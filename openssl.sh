4c720a7efd719f4a665a9a5061042dd73f187c56
Mario Weiler <mariow@arizona.edu>
2024-01-31 13:46:29 -0700

    ip address to ip range coverter

--- openssl.sh
+++ openssl.sh
@@ -21,10 +21,35 @@ then
     exit 1
 fi
 
+# Convert the provided IP address to a range
+ip=$1
+IFS='.' read -ra ADDR <<< "$ip"
+range="${ADDR[0]}.${ADDR[1]}.${ADDR[2]}.0-${ADDR[0]}.${ADDR[1]}.${ADDR[2]}.255"
+
+is_valid_ip()
+{
+    local ip=$1
+    local IFS='.' parts=($ip) count=${#parts[@]}
+    if ((count != 4)); then return 1; fi
+    for part in "${parts[@]}"; do
+        if ((part < 0 || part > 255)); then return 1; fi
+    done
+    return 0
+}
+
+# Then, before calling ip_to_int, check if the IP is valid
+# if is_valid_ip "$ip"; then
+#     ip_to_int "$ip"
+# else
+#     echo "Invalid IP address: $ip"
+#     exit 1
+# fi
+
 # Function to convert IP to integer
 ip_to_int()
 {
     local IFS='.' ip=($1) i
+    ip=()
     for i in {0..3}; do ip[i]=$((ip[i] << (24 - 8 * i))); done
     echo $((ip[0] | ip[1] | ip[2] | ip[3]))
 }
